c4004445803c079702e4c3785e5ee9ad873b83d2
raj_poojara <rspoojara123@gmail.com>
2024-04-07 12:39:21 +0530

    Demo Branch : This branch contains the  changes related to the clss based structure
    Signed-off-by: raj_poojara <rspoojara123@gmail.com>

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi(name: str) -> None:
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Demo:
+    @staticmethod
+    def print_hi(name: str) -> None:
+        # Use a breakpoint in the code line below to debug your script.
+        print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    demo = Demo()
+    demo.print_hi('PyCharm')
     print("Hello There! This is just a demo script!!", flush=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
